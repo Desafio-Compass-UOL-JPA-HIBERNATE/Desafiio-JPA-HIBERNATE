3e791d681a6dff4c864b57b49e248e2886e76600
Thalison <Tec.thalison@gmail.com>
2024-02-02 21:37:35 -0300

    feat: Implementation of the IProduct interface to ProuctDAO

--- src/main/java/br/com/ecommerce/dto/ProductDAO.java
+++ src/main/java/br/com/ecommerce/dto/ProductDAO.java
@@ -1,4 +1,38 @@
 package br.com.ecommerce.dto;
 
-public class ProductDAO {
+import br.com.ecommerce.domain.Product;
+import br.com.ecommerce.dto.interfaces.IProductDAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class ProductDAO implements IProductDAO {
+
+    private EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public Product create() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Product update() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Product read() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(Integer id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void delete() throws Exception {
+
+    }
 }
--- src/main/java/br/com/ecommerce/dto/interfaces/IProductDAO.java
+++ src/main/java/br/com/ecommerce/dto/interfaces/IProductDAO.java
@@ -2,10 +2,13 @@ package br.com.ecommerce.dto.interfaces;
 
 import br.com.ecommerce.domain.Product;
 
+import java.util.List;
+
 public interface IProductDAO {
 
     public Product create() throws Exception;
     public Product update() throws Exception;
     public Product read() throws Exception;
+    public Product getProduct(Integer id) throws Exception;
     public void delete() throws Exception;
 }
