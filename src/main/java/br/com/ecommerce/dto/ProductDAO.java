efa8e74855eca8acf940f0cf7e3e45c2336819b3
Thalison <Tec.thalison@gmail.com>
2024-02-06 15:44:05 -0300

    docs: Added javadoc to ProdutoDAO

--- src/main/java/br/com/ecommerce/dto/ProductDAO.java
+++ src/main/java/br/com/ecommerce/dto/ProductDAO.java
@@ -11,6 +11,10 @@ import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+/**
+ * Classe DAO
+ */
+
 public class ProductDAO implements IProductDAO {
 
   
@@ -21,6 +25,10 @@ public class ProductDAO implements IProductDAO {
 
 
     //Adicionar validação
+
+    /**
+     * Constructor
+     */
     public ProductDAO() {
         try {
             emf = Persistence.createEntityManagerFactory("persiste-ecommerce"); // Aqui diz a unidade de persistência especificada no arquivo persistance.xml
@@ -30,12 +38,20 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+    /**
+     * Method that terminates the EntityManager connection
+     */
     public void close() { //Closes database connection
         em.close();
         emf.close();
     }
 
-    //Testing Privilege
+    /**
+     * This method adds the product
+     * @param product Object product
+     * @return Returns the product when it is able to perform the survey
+     * @throws Exception
+     */
     @Override
     public Product create(Product product) throws Exception {
         try {
@@ -49,6 +65,13 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+    /**
+     * Method of updating product
+     * @param p Object product
+     * @param id Product ID
+     * @return Retorna o produto ao realizar o update
+     * @throws Exception
+     */
     @Override
     public Product update(Product p, Integer id) throws Exception {
         try {
@@ -69,6 +92,12 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+
+    /**
+     * Method that lists products
+     * @return Returns the list of products that are saved in the database
+     * @throws Exception
+     */
     @Override
     public List<Product> listAll() throws Exception {
         try{
@@ -80,6 +109,13 @@ public class ProductDAO implements IProductDAO {
 
     }
 
+    /**
+     *Method that searches by ID
+     * @param id Product ID
+     * @return It will return the product found with the ID entered in the parameter
+     * @throws Exception
+     */
+
     @Override
     public Product findById(Integer id) throws Exception {
         try {
@@ -89,6 +125,12 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+    /**
+     *
+     * @param name Product name
+     * @return It will return the product found with the name entered in the parameter
+     * @throws Exception
+     */
     public Product findByName(String name) throws Exception {
         try {
             TypedQuery<Product> query = em.createQuery("SELECT p FROM Product p WHERE p.name = :name", Product.class);
@@ -99,6 +141,11 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+    /**
+     *Method for deleting the product
+     * @param id Product ID that will be deleted
+     * @throws Exception
+     */
     @Override
     public void delete(Integer id) throws Exception {
         try {
@@ -114,6 +161,11 @@ public class ProductDAO implements IProductDAO {
         }
     }
 
+    /**
+     *
+     * @param productName
+     * @throws ProductAlreadyExistsException
+     */
 	private void checkIfProductExists(String productName) throws ProductAlreadyExistsException {
 		TypedQuery<Long> query = em.createQuery("SELECT COUNT(p) FROM Product p WHERE p.name = :name", Long.class);
 		query.setParameter("name", productName);//compara no banco de dados se o produto com mesmo nome já existe
