4ee0709438d4ef7dabb159651ca9a49d0251ef6e
Thalison <Tec.thalison@gmail.com>
2024-02-06 15:47:26 -0300

    docs: Generated a new application javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.ecommerce.dto"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.ecommerce"},{"l":"br.com.ecommerce.dto"}];updateSearchResults();
